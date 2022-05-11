163ab0ae0db324729fa0f531dc0ff5399f89a72a
Santiago <fsgreco@hey.com>
2022-05-11 17:14:23 +0200

    Solution 3 - Icon Input

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,46 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+	const iconSize = size === "small" ? 16 : 24;
+  return (
+    <MainWrapper measures={{ width,size }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+			<TheIconWrapper>
+      	<Icon id={icon} size={iconSize} />
+			</TheIconWrapper>
+      <TextInput placeholder={placeholder} type="text" />
+
+    </MainWrapper>
+  );
 };
 
 export default IconInput;
+
+/* STYLES */
+
+const MainWrapper = styled.label`
+	--coefix: ${({measures}) => measures.size === "small" ? 6 : 9 };
+	--width: ${p => p.measures.width }px;
+
+	display: block;
+	isolation: isolate;
+	position: relative;
+`
+
+const TheIconWrapper = styled.div`
+  position: absolute;
+  top: 2px;
+  left: 0;
+  z-index: 99;
+  
+`;
+const TextInput = styled.input`
+	width: var(--width);
+	height: calc( var(--coefix) * 4px );
+	border: 0;
+	border-bottom: 1px solid ${COLORS.black};
+	padding: 5px 5px 5px 34px;
+
+	position: relative;
+	z-index: 1;
+`
