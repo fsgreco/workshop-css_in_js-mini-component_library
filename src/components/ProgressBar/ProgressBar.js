137dac6b4b733168ba0e731c7af955904392c6ab
Santiago <fsgreco@hey.com>
2022-05-11 15:13:46 +0200

    Solution 1 - Progress Bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,45 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+	const heights = new Map([
+		["small", "8px" ], 
+		["medium", "12px" ], 
+		["large", "24px"]
+	]);
+
+  return (
+    <TheBar 
+			role="progressbar" 
+			aria-valuenow={value} 
+			ht={ heights.get(size) }
+		>
+      <InternalProgress length={value}>
+				<VisuallyHidden>{value}</VisuallyHidden>
+			</InternalProgress>
+    </TheBar>
+  );
 };
 
 export default ProgressBar;
+
+/* STYLES */
+
+const TheBar = styled.div`
+	border-radius: 8px;
+  height: ${p => p.ht};
+	/* Magic number here, p.ht === '24px' means it's "large" - ref heights Map above: */
+	padding: ${p => p.ht === '24px' ? '4px' : '0px'};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+`;
+
+const ProgressBase = styled.div`
+  height: 100%;
+  text-align: center;
+  background-color: ${COLORS.primary};
+  color: ${COLORS.primary};
+`
+
+const InternalProgress = styled(ProgressBase)`
+  border-radius: 8px ${p => p.length === 100 ? '8px' : '0px 0px' } 8px;
+  width: ${(p) => p.length}%;
+`
